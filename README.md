bac7574a9e972fa40bac7b26a2134f3faf829413
Mariusz Nowak <mariusz@medikoo.com>
2013-09-28 20:20:32 +0200

    Initial

---
+++ README.md
@@ -0,0 +1,10 @@
+# git-branch-deploy
+## Setup repository branch for deployment
+
+### Installation
+
+	$ npm install -g git-branch-deploy
+
+### Usage
+
+  $ git-branch-deploy some-remote/fix-issue
---
+++ index.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var copy       = require('es5-ext/object/copy')
+  , isCallable = require('es5-ext/object/is-callable')
+  , callable   = require('es5-ext/object/valid-callable')
+  , ExecBatch  = require('exec-batch');
+
+module.exports = function (branch/*, options, cb*/) {
+	var options = arguments[1], cb = arguments[2], remote, batch;
+	if (cb != null) {
+		callable(cb);
+		options = Object(options);
+	} else if (isCallable(options)) {
+		cb = options;
+		options = {};
+	} else {
+		options = Object(options);
+	}
+	if (options.remote != null) {
+		remote = options.remote;
+		options = copy(options);
+		delete options.remote;
+	}
+	batch = new ExecBatch(options);
+
+	batch.add('git reset');
+	batch.add('git checkout .');
+	batch.add('git clean -df');
+	batch.add('git checkout master');
+	batch.add('git pull');
+	batch.add('git fetch' + (remote ? (' ' + remote) : ''));
+	batch.add('git merge --no-commit --no-ff ' +
+		(remote ? (remote + '/') : '') + branch);
+	batch.add('git reset');
+
+	batch.start(cb);
+};
