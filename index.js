af285a83e12907764a7adf1a409ce5cec43b7476
Mariusz Nowak <mariusz@medikoo.com>
2013-10-17 13:18:22 +0200

    Fix merge command

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ module.exports = function (branch/*, options, cb*/) {
 	batch.add('git checkout master');
 	batch.add('git pull');
 	batch.add('git fetch ' + remote + ' ' + branch);
-	batch.add('git merge --no-commit --no-ff ' + remote + ' ' + branch);
+	batch.add('git merge --no-commit --no-ff ' + remote + '/' + branch);
 	batch.add('git reset');
 
 	batch.start(cb);
